d52af493da9e4dd7720e4c8b1180586f554f4fca
Piero Alvaro Salazar Calle <54618161+Pieromental@users.noreply.github.com>
2024-06-19 04:58:04 -0500

    CONFIGURACION DE CIRCUIT BEAKER PARA MANEJO DE ERRORES

--- src/main/java/com/example/similar_products/controller/ProductController.java
+++ src/main/java/com/example/similar_products/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.example.similar_products.controller;
 
 import com.example.similar_products.model.ProductDetail;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
 @RestController
@@ -20,22 +22,22 @@ public class ProductController {
     private static final String SIMILAR_IDS_URL = "http://localhost:3001/product/{productId}/similarids";
     private static final String PRODUCT_DETAIL_URL = "http://localhost:3001/product/{productId}";
 
+    @CircuitBreaker(name = "similarProducts", fallbackMethod = "fallbackGetSimilarProducts")
     @GetMapping("/product/{productId}/similar")
     public List<ProductDetail> getSimilarProducts(@PathVariable String productId) {
-
         String[] similarIds = restTemplate.getForObject(SIMILAR_IDS_URL, String[].class, productId);
 
- 
-        return Arrays.stream(similarIds)
-                .map(id -> {
-                    try {
-                        return restTemplate.getForObject(PRODUCT_DETAIL_URL, ProductDetail.class, id);
-                    } catch (Exception e) {
-                       
-                        return null;
-                    }
-                })
-                .filter(productDetail -> productDetail != null) 
+        List<CompletableFuture<ProductDetail>> futures = Arrays.stream(similarIds)
+                .map(id -> CompletableFuture.supplyAsync(() -> restTemplate.getForObject(PRODUCT_DETAIL_URL, ProductDetail.class, id)))
                 .collect(Collectors.toList());
+
+        return futures.stream()
+                .map(CompletableFuture::join)
+                .collect(Collectors.toList());
+    }
+
+    public List<ProductDetail> fallbackGetSimilarProducts(String productId, Throwable throwable) {
+        // Lógica de fallback en caso de fallo
+        return List.of();
     }
 }
